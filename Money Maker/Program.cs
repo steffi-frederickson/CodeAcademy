2e67d700ccff81871d0d26af8c57de1e347733c1
steffi-frederickson <106619557+steffi-frederickson@users.noreply.github.com>
2022-05-31 14:29:38 -0500

    Add files via upload

---
+++ Money Maker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Enter an amount to convert to coins.");
+            string firstAmount = Console.ReadLine();
+            double total = Convert.ToDouble(firstAmount);
+            Console.WriteLine($"{total} is equal to ...");
+            int goldValue = 10;
+            int silverValue = 5;
+            double goldCoins = Math.Floor(total / goldValue);
+            double leftOver = total % goldValue;
+
+            double silverCoins = Math.Floor(leftOver / silverValue);
+            double remainder = leftOver % silverValue;
+
+            Console.WriteLine($"Gold coins:{goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins:{remainder}");
+        }
+    }
+}
